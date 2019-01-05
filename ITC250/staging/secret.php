3563f14e8cbc24be79fd6afdf9ecf46a77311e46
Jess <jessallen6@hotmail.com>
2019-01-04 19:05:09 -0800

    Create secret.php

---
+++ ITC250/staging/secret.php
@@ -0,0 +1,8 @@
+<?php
+
+define ('DBHOST', 'localhost');
+define ('DBNAME', 'phpapp');
+define ('DBUSER', 'root');
+define ('DBPASSWORD', 'password');
+
+//this will not connect as I have entered generic info
